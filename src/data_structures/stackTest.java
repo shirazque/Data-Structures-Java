3a3c2d564d0b86e68c097d089f0391aec19cc460
Shiraz <shirazqmmos@gmail.com>
2024-08-29 14:57:08 -0400

    Updated int to integer to avoid syntax errors

--- src/data_structures/stackTest.java
+++ src/data_structures/stackTest.java
@@ -5,7 +5,7 @@
  * Author: Shiraz Qasmi
  * ID: 169036233
  * Email: qasm6233@mylaurier.ca
- * __updated__ = "2024-08-28"
+ * __updated__ = "2024-08-29"
  * -------------------------------------------------------
  */
 
@@ -14,6 +14,7 @@ package data_structures;
 public class stackTest {
     public static void main(String[] args) {
         stack testStack = new stack();
+        stack additionalStack = new stack();
         
         // Pushing elements onto the Stack
         testStack.push(0);
@@ -29,12 +30,12 @@ public class stackTest {
         System.out.println("");
 
         // Popping from the Stack
-        int val = testStack.pop();
+        Integer val = testStack.pop();
         System.out.println();
         System.out.println("Popped value from Stack: " + val);
 
         // Peeking from the Stack
-        int peek_val = testStack.peek();
+        Integer peek_val = testStack.peek();
         System.out.println("Peeked value from the Stack: " + peek_val);
 
         // Printing out the contents of the Stack
